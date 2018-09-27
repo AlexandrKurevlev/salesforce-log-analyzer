b825ba6f77552960ef18bb4a8f86f831b6b8785b
Alexandr Kurevlev <alexandrkurevlev@gmail.com>
2018-09-27 23:15:21 +0300

    init commit

---
+++ src/main/java/com/example/App.java
@@ -0,0 +1,13 @@
+package com.example;
+
+import static spark.Spark.*;
+
+public class App {
+
+    public static void main(String[] args) {
+        staticFileLocation("/public");
+
+        init();
+    }
+
+}
